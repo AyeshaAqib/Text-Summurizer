a7550b6d636faaf9fc68ffe9c2822cd149b6f6e0
Ayesha Aqib <ayegroup92@gmail.com>
2025-08-02 19:49:33 +0500

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summurizer"
+AUTHOR_USER_NAME = 'AyeshaAqib'
+SRC_REPO = 'textSummurizer'
+AUTHOR_EMAIL = 'aliaqib8989@gmail.com'
+
+
+setuptools.setup(
+    name =SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python packages for NLP app",
+    long_description=long_description,
+    long_description_content ="text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug_tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        
+    }
+)
+
